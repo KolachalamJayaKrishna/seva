3323ffc4e55e468f1906b1d16882754f6c3fd390
KolachalamJayaKrishna <127821673+KolachalamJayaKrishna@users.noreply.github.com>
2025-03-27 11:44:11 +0530

    Location_update

--- src/Branch.js
+++ src/Branch.js
@@ -15,31 +15,48 @@ const Branches = () => {
         <div className="branch-card">
           <h2>Tarnaka</h2>
           <p>12-13-289, St.9, Lane-2, St.Ann's School Road, Tarnaka, Hyderabad 500017 <br/> Daily 8:30am - 5:30pm <br/> 7207014049,040-42604049</p>
-          
+          <br/><br/>
+          <div className="links-social"style={{justifyContent:"center"}}>
+              <a href="https://maps.app.goo.gl/RkQ8vqEuV6fMMvj46" target="_blank" rel="noopener noreferrer">Location</a> </div>
         </div>
         <div className="branch-card">
           <h2>Sanathnagar</h2>
           <p>SRT 415, Opp. Toyota Lane,second left lane Sanathnagar, Hyderabad 500018 <br/> Daily 8:30am - 5:30pm<br/> 7842074049,7416014049</p>
           <img src={sanathnagar} alt="Sanathnagar" />
+          <br/><br/>
+          <div className="links-social"style={{justifyContent:"center"}}>
+              <a href="https://maps.app.goo.gl/RJstonisXwK8hiQn8" target="_blank" rel="noopener noreferrer">Location</a> </div>
         </div>
         <div className="branch-card">
           <h2>Kukatpally</h2>
           <p>H.No: 15-21-211, Balaji Nagar, Kukatpally, Hyderabad 500072 <br/>Daily<br/> 8:30am - 5:30pm<br/> 9000654049</p>
           <img src={kukatpally} alt="Kukatpally" />
+          <br/><br/>
+          <div className="links-social"style={{justifyContent:"center"}}>
+              <a href="https://maps.app.goo.gl/NTzpE2YnrteHVycZA" target="_blank" rel="noopener noreferrer">Location</a> </div>
         </div>
         <div className="branch-card">
           <h2>Dammaiguda</h2>
           <p>H.No: 9-18/5, Plot No: 13-S.Part, Sy.No: 597, Gayathri Nagar, Dammaiguda, Hyderabad <br/>Daily 8:30am - 5:30pm<br/> 9666309705</p>
           <img src={dammaiguda} alt="Dammaiguda" />
+          <br/><br/>
+          <div className="links-social"style={{justifyContent:"center"}}>
+              <a href="https://maps.app.goo.gl/t7yn8pVKhfGq4RRE7" target="_blank" rel="noopener noreferrer">Location</a> </div>
         </div>
         <div className="branch-card">
           <h2>New Nagole</h2>
           <p>Samathapuri Colony, Road No: 7, Plot No: 80, New Nagole, Hyderabad<br/> Daily 8:30am - 5:30pm<br/> 7207094049</p>
           <img src={nagole} alt="Nagole" />
+          <br/><br/>
+          <div className="links-social"style={{justifyContent:"center"}}>
+              <a href="https://maps.app.goo.gl/6rSt5wvR97wmrcnu7" target="_blank" rel="noopener noreferrer">Location</a> </div>
         </div>
         <div className="branch-card">
           <h2>Vijayawada</h2>
           <p>Near SBI, No: 130, Addankivari Street, Current Nagar, Ramavarappadu, Vijayawada, Andhra Pradesh, Daily<br/> 7842250270</p>
+          <br/><br/>
+          <div className="links-social"style={{justifyContent:"center"}}>
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Location</a> </div>
         </div>
       </div>
     </div>
